c2f51ab0af15e6002150680e2ceb04e344d302a0
Andrew Johnson <ajohnson.uva@gmail.com>
2019-03-12 14:26:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(array, string) {
+  return array.filter(function (name) { return name.toLowerCase() === string.toLowerCase(); });
+};
+
+function fuzzyMatch(array, string) {
+  return array.filter(function (name) { return name.startsWith(string); })
+};
+
+function matchName(array, string) {
+  return array.filter(function (array) { return array.name === string; });
+};
