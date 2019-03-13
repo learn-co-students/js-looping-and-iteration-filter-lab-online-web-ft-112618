a783973ce88dd24bfbed1e80fb059c3d205ce257
Molly McCarron <mccarronmollye@gmail.com>
2019-03-13 17:19:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file
+function findMatching(drivers, string){
+  return drivers.filter(function (driverName) {
+    return driverName.toLowerCase() === string.toLowerCase()});
+}
+
+
+function fuzzyMatch(drivers, partialName){
+  let lengthOfName = partialName.length;
+  return drivers.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName; });
+}
+
+function matchName(drivers, string){
+  return drivers.filter(function (driver) {
+    return driver.name.toLowerCase() === string.toLowerCase()});
+}
