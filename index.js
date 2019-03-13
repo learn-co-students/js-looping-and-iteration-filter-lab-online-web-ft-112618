be14890020cdf26806c79890ef6fa2658067e979
Nick Inman <ninman@Nicks-MBP.fios-router.home>
2019-03-13 14:07:51 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(function (driverName) { return driverName.toLowerCase() === name.toLowerCase() })
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(function (driverName) { return driverName.substr(0, letters.length) === letters })
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(function (driverName) { return driverName.name === name })
+}
